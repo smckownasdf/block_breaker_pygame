a75f31981b654486038f37b05a469134aa41f30a
Scott McKown <scottmckown@fastmail.com>
2019-07-24 17:47:46 -0700

    Added collision event between paddle and ball rects so that paddle will return ball when hit. Removed bottom 'barrier' and instead returned ball to top of screen when ball reaches bottom of screen. Added cushion when reversing ball to prevent ball from getting stuck on walls - i.e., reversing direction at each successive render because the first render beyond the barrier, where the direction reversal took place, happened to reach farther than expected.

--- block_breaker_pygame.py
+++ block_breaker_pygame.py
@@ -3,7 +3,12 @@ import sys, pygame
 # Static Variables
 screen_size = width, height = 1600, 900
 ball_speed = [400, 600]
-black = 0, 0, 0
+black = (0, 0, 0, 255)
+transparent = (0, 0, 0, 0)
+paddle_color = pygame.Color(154, 100, 220, a=200)
+
+paddle_move_dict = {pygame.K_LEFT	: [-1, 0],
+					pygame.K_RIGHT	: [ 1, 0]}
 
 class Ball(object):
 	def __init__(self, speed, screen):
@@ -15,12 +20,45 @@ class Ball(object):
 	def move(self, screen_rect, dt):
 		self.rect = self.rect.move(self.speed[0]*dt, self.speed[1]*dt)
 		if self.rect.left < 0 or self.rect.right > screen_rect.right:
-			self.speed[0] = -self.speed[0]
-		if self.rect.top < 0 or self.rect.bottom > screen_rect.bottom:
-			self.speed[1] = -self.speed[1]
+			self.speed[0] = -self.speed[0] - 5
+		if self.rect.top < 0:
+			self.speed[1] = -self.speed[1] + 5
+		self.lost_ball(screen_rect)
+
+	def lost_ball(self, screen_rect):
+		if self.rect.top > screen_rect.bottom:
+			self.rect.top = 0
 
 	def draw(self, surface):
 		surface.blit(self.image, self.rect)
+
+class Paddle(object):
+	size = (150, 20)
+	def __init__(self, posx, posy):
+		self.image = self.create_paddle()
+		self.rect = self.image.get_rect(center=(posx, posy))
+		self.true_pos = list(self.rect.center)
+		self.move_speed = 20
+
+	def create_paddle(self):
+		image = pygame.Surface(Paddle.size).convert_alpha()
+		image.fill(transparent)
+		img_rect = image.get_rect()
+		pygame.draw.rect(image, paddle_color, img_rect)
+		return image
+
+	def update(self, pressedkeys, screen_rect, dt):
+		if pressedkeys[pygame.K_LEFT]:
+			if self.rect.left > 0:
+				self.true_pos[0] -= self.move_speed
+		if pressedkeys[pygame.K_RIGHT]:
+			if self.rect.right < screen_rect.right:
+				self.true_pos[0] += self.move_speed
+		self.rect.center = self.true_pos
+
+	def draw(self, surface):
+		surface.blit(self.image, self.rect)
+
 
 class App(object):
 	def __init__(self):
@@ -31,6 +69,8 @@ class App(object):
 		self.done = False
 		self.keys = pygame.key.get_pressed()
 		self.ball = Ball(ball_speed, self.screen)
+		self.paddle_offset = 30
+		self.paddle = Paddle(self.screen_rect.centerx, self.screen_rect.bottom - self.paddle_offset)
 
 	def event_loop(self):
 		for event in pygame.event.get():
@@ -39,13 +79,19 @@ class App(object):
 			elif event.type in (pygame.KEYDOWN, pygame.KEYUP):
 				self.keys = pygame.key.get_pressed()
 
+	def paddle_hit(self):
+		if pygame.sprite.collide_rect(self.paddle, self.ball):
+			self.ball.speed[1] = -self.ball.speed[1] - 5
+
 	def render(self):
 		self.screen.fill(black)
 		self.ball.draw(self.screen)
+		self.paddle.draw(self.screen)
 		pygame.display.update()
 
 	def update(self, dt):
 		self.ball.move(self.screen_rect, dt)
+		self.paddle.update(self.keys, self.screen_rect, dt)
 
 	def main_loop(self):
 		dt = 0
@@ -54,6 +100,7 @@ class App(object):
 			self.event_loop()
 			self.update(dt)
 			self.render()
+			self.paddle_hit()
 			dt = self.clock.tick(self.fps)/1000.0
 
 def main():
