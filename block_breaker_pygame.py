0db150e6f4e8ad7df3032d7e02a2c3d06d83a53b
Scott McKown <scottmckown@fastmail.com>
2019-07-28 10:29:05 -0700

    Implemented pygame.sprite.Sprite after reading about organizing game objects in pygame; made block destroyable
    Implemented pygame.sprite.Sprite; removed draw methods from Paddle, Block and Ball classes due to irrelevance with pygame.sprite methods; renamed and modified Ball.move method to Ball.update to make use of pygame.sprite; modified Paddle.update and App.event_loop to work with each other more cleanly; refactored constraints on paddle movement to App.contain_paddle method; Made create_background method within App class to replace unnecessarily global 'black' variable; Created App.bounce method to more cleanly run Ball.bounce methods within App.update

--- block_breaker_pygame.py
+++ block_breaker_pygame.py
@@ -3,11 +3,11 @@ import sys, pygame
 # Static Variables
 screen_size = width, height = 1600, 900
 ball_speed = [400, 600]
-black = (0, 0, 0, 255)
 transparent = (0, 0, 0, 0)
 
-class Ball(object):
+class Ball(pygame.sprite.Sprite):
 	def __init__(self, speed, screen):
+		pygame.sprite.Sprite.__init__(self) # Call Sprite initializer
 		self.image = pygame.transform.scale(pygame.image.load("ball.png").convert(), (20,20)).convert_alpha()
 		self.rect = self.image.get_rect()
 		self.speed = speed
@@ -28,6 +28,8 @@ class Ball(object):
 
 	def bounce(self, collider):
 		if self.rect.colliderect(collider):
+			if collider.__class__.__name__ == "Block":
+				collider.hit()
 			if self.speed[1] >= 0: # ball is moving down
 				if self.speed[0] >= 0: # ball is moving right
 					# Corner hit
@@ -79,9 +81,10 @@ class Ball(object):
 						self.bouncex()
 						self.rect.left = collider.rect.right
 
+	def update(self):
+		self.rect = self.rect.move(self.speed[0]*App.dt, self.speed[1]*App.dt)
 
-	def move(self, screen_rect, dt):
-		self.rect = self.rect.move(self.speed[0]*dt, self.speed[1]*dt)
+	def screen_edges(self, screen_rect):
 		if self.rect.left < 0:
 			self.bouncex()
 			self.rect.left = 0
@@ -97,13 +100,11 @@ class Ball(object):
 		if self.rect.top > screen_rect.bottom:
 			self.rect.top = 0
 
-	def draw(self, surface):
-		surface.blit(self.image, self.rect)
-
-class Paddle(object):
+class Paddle(pygame.sprite.Sprite):
 	size = (150, 20)
 	paddle_color = pygame.Color(154, 100, 220, a=200)
 	def __init__(self, posx, posy):
+		pygame.sprite.Sprite.__init__(self)
 		self.image = self.create_paddle()
 		self.rect = self.image.get_rect(center=(posx, posy))
 		self.true_pos = list(self.rect.center)
@@ -116,23 +117,21 @@ class Paddle(object):
 		pygame.draw.rect(image, Paddle.paddle_color, img_rect)
 		return image
 
-	def update(self, pressedkeys, screen_rect, dt):
-		if pressedkeys[pygame.K_LEFT]:
-			if self.rect.left > 0:
-				self.true_pos[0] -= self.move_speed
-		if pressedkeys[pygame.K_RIGHT]:
-			if self.rect.right < screen_rect.right:
-				self.true_pos[0] += self.move_speed
+	def update(self):
 		self.rect.center = self.true_pos
+		if App.pressed_left:
+			self.true_pos[0] -= self.move_speed
+		if App.pressed_right:
+			self.true_pos[0] += self.move_speed
 
-	def draw(self, surface):
-		surface.blit(self.image, self.rect)
-
-class Block(object):
+class Block(pygame.sprite.Sprite):
 	size = (350, 80)
 	block_color = pygame.Color(220,114,42,a=200)
 	one_hit = 1
+	two_hit = 2
+	three_hit = 3
 	def __init__(self, posx, posy, hitmax):
+		pygame.sprite.Sprite.__init__(self)  # Call sprite initializer
 		self.image = self.create_block()
 		self.rect = self.image.get_rect(center=(posx, posy))
 		self.hit_count = 0
@@ -145,13 +144,21 @@ class Block(object):
 		pygame.draw.rect(image, Block.block_color, img_rect)
 		return image
 
-	def draw(self, surface):
-		surface.blit(self.image, self.rect)
+	def hit(self):
+		self.hit_count += 1
+		print(self.hit_count)
+		self.destroy()
 
-	def destroy(self, hitmax):
-		pass
+	def destroy(self):
+		if self.hit_count == self.hit_max:
+			self.kill()
+			print("killed?")
+
 
 class App(object):
+	pressed_left = False
+	pressed_right = False
+	dt = 0
 	def __init__(self):
 		self.screen = pygame.display.get_surface()
 		self.screen_rect = self.screen.get_rect()
@@ -162,40 +169,63 @@ class App(object):
 		self.ball = Ball(ball_speed, self.screen)
 		self.paddle_offset = 30
 		self.paddle = Paddle(self.screen_rect.centerx, self.screen_rect.bottom - self.paddle_offset)
-		self.block = Block(500, 100, Block.one_hit)
+		self.block = Block(500, 100, Block.three_hit)
+		self.all_sprites = pygame.sprite.RenderPlain((self.ball, self.block, self.paddle))
+		self.background = self.create_background()
+
+	def create_background(self):
+		self.background = pygame.Surface(self.screen.get_size())
+		self.background = self.background.convert()
+		self.background.fill((50,50,50))
+		return self.background
+
 
 	def event_loop(self):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				self.done = True
-			elif event.type in (pygame.KEYDOWN, pygame.KEYUP):
-				self.keys = pygame.key.get_pressed()
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					App.pressed_left = True
+				if event.key == pygame.K_RIGHT:
+					App.pressed_right = True
+			elif event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT:
+					App.pressed_left = False
+				elif event.key == pygame.K_RIGHT:
+					App.pressed_right = False
 
-	def render(self):
-		self.screen.fill(black)
-		self.ball.draw(self.screen)
-		self.paddle.draw(self.screen)
-		self.block.draw(self.screen)
-		pygame.display.update()
+	def contain_paddle(self): # Should probably be in Paddle, but I didn't want to bother connecting screen_rect to Paddle class
+		if self.paddle.rect.left <= 0:
+			App.pressed_left = False
+		elif self.paddle.rect.right >= self.screen_rect.right:
+			App.pressed_right = False
 
-	def update(self, dt):
-		self.ball.move(self.screen_rect, dt)
-		self.paddle.update(self.keys, self.screen_rect, dt)
+	def bounce(self):
+		if self.block in self.all_sprites:
+			self.ball.bounce(self.block)
+		self.ball.bounce(self.paddle)		
+
+	def update(self):
+		self.bounce()
+		self.all_sprites.update()
+		self.contain_paddle()
+		self.screen.blit(self.background, (0,0))
+		self.all_sprites.draw(self.screen)
+		pygame.display.flip()
 
 	def main_loop(self):
-		dt = 0
 		self.clock.tick(self.fps)
 		while not self.done:
 			self.event_loop()
-			self.update(dt)
-			self.render()
-			self.ball.bounce(self.block)
-			self.ball.bounce(self.paddle)
-			dt = self.clock.tick(self.fps)/1000.0
+			self.update()
+			self.ball.screen_edges(self.screen_rect) # Again, like in App.contain_paddle()...Didn't want to pass screen_rect to ball, but probably should
+			App.dt = self.clock.tick(self.fps)/1000.0
 
 def main():
 	pygame.init()
 	pygame.display.set_mode(screen_size)
+	pygame.display.set_caption('Pygame Block Breaker')
 	App().main_loop()
 	pygame.quit()
 	sys.exit()
