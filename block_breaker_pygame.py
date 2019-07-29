8cbcfc54caba6176e1b38bb90385b6c377f8b1b6
Scott McKown <scottmckown@fastmail.com>
2019-07-29 14:18:48 -0700

    Implemented Level class (but no level loader); cleaned up and organized a whole bunch of code
    Changed screen height from 900 to 800 to better accommodate 32px sprite resolution; Moved Ball.screen_edges call to Ball.update to reflect common sense; Moved contain_paddle method to Paddle class and called it within Paddle.update for similar common-sense-based reasons; Moved Paddle.size and Paddle.paddle_color into Paddle.__init__ and did the same thing with related block variables because there's no need for them to be used outside of their respective classes; Created Block.determine_color method to add color variance based on block strength; Removed print statements from Block.hit and Block.destroy because it's clear now that they work; Created Level class, and created collision_check, choose_level and build_level methods within it; Moved create_background method to Level class; Removed self.keys from App.__init__ because it's unused; added self.level to App.__init__; Removed App.bounce method and replaced its funcitonality with Level.collision_check; Updated App.update method to reflect method changes.

--- block_breaker_pygame.py
+++ block_breaker_pygame.py
@@ -1,18 +1,22 @@
 import sys, pygame
 
-# Static Variables
-screen_size = width, height = 1600, 900
+# Global Variables
+screen_size = width, height = 1600, 800
 ball_speed = [400, 600]
 transparent = (0, 0, 0, 0)
+pixelx = 64
+pixely = 32
 
 class Ball(pygame.sprite.Sprite):
-	def __init__(self, speed, screen):
+	def __init__(self, posx, posy):
 		pygame.sprite.Sprite.__init__(self) # Call Sprite initializer
 		self.image = pygame.transform.scale(pygame.image.load("ball.png").convert(), (20,20)).convert_alpha()
 		self.rect = self.image.get_rect()
-		self.speed = speed
-		self.screen = screen
+		self.speed = ball_speed
+		self.screen = pygame.display.get_surface()
 		self.corner_threshhold = 1.5
+		self.screen = pygame.display.get_surface()
+		self.screen_rect = self.screen.get_rect()
 
 	def bouncex(self):
 		if self.speed[0] >= 0:
@@ -83,41 +87,51 @@ class Ball(pygame.sprite.Sprite):
 
 	def update(self):
 		self.rect = self.rect.move(self.speed[0]*App.dt, self.speed[1]*App.dt)
+		self.screen_edges()
 
-	def screen_edges(self, screen_rect):
+	def screen_edges(self):
 		if self.rect.left < 0:
 			self.bouncex()
 			self.rect.left = 0
-		elif self.rect.right > screen_rect.right:
+		elif self.rect.right > self.screen_rect.right:
 			self.bouncex()
-			self.rect.right = screen_rect.right
+			self.rect.right = self.screen_rect.right
 		if self.rect.top < 0:
 			self.bouncey()
 			self.rect.top = 0
-		self.lost_ball(screen_rect)
+		self.lost_ball(self.screen_rect)
 
 	def lost_ball(self, screen_rect):
 		if self.rect.top > screen_rect.bottom:
 			self.rect.top = 0
 
 class Paddle(pygame.sprite.Sprite):
-	size = (150, 20)
-	paddle_color = pygame.Color(154, 100, 220, a=200)
 	def __init__(self, posx, posy):
 		pygame.sprite.Sprite.__init__(self)
+		self.size = (150, 20)
+		self.paddle_color = pygame.Color(154, 100, 220, a=200)
 		self.image = self.create_paddle()
 		self.rect = self.image.get_rect(center=(posx, posy))
 		self.true_pos = list(self.rect.center)
 		self.move_speed = 20
+		self.screen = pygame.display.get_surface()
+		self.screen_rect = self.screen.get_rect()
 
 	def create_paddle(self):
-		image = pygame.Surface(Paddle.size).convert_alpha()
+		image = pygame.Surface(self.size).convert_alpha()
 		image.fill(transparent)
 		img_rect = image.get_rect()
-		pygame.draw.rect(image, Paddle.paddle_color, img_rect)
+		pygame.draw.rect(image, self.paddle_color, img_rect)
 		return image
 
+	def contain_paddle(self): 
+		if self.rect.left <= 0:
+			App.pressed_left = False
+		elif self.rect.right >= self.screen_rect.right:
+			App.pressed_right = False
+
 	def update(self):
+		self.contain_paddle()
 		self.rect.center = self.true_pos
 		if App.pressed_left:
 			self.true_pos[0] -= self.move_speed
@@ -125,35 +139,155 @@ class Paddle(pygame.sprite.Sprite):
 			self.true_pos[0] += self.move_speed
 
 class Block(pygame.sprite.Sprite):
-	size = (350, 80)
-	block_color = pygame.Color(220,114,42,a=200)
 	one_hit = 1
 	two_hit = 2
 	three_hit = 3
 	def __init__(self, posx, posy, hitmax):
 		pygame.sprite.Sprite.__init__(self)  # Call sprite initializer
-		self.image = self.create_block()
-		self.rect = self.image.get_rect(center=(posx, posy))
 		self.hit_count = 0
 		self.hit_max = hitmax
+		self.size = (62, 30)
+		self.block_color = self.determine_color()
+		self.image = self.create_block()
+		self.rect = self.image.get_rect(center=(posx, posy))
+
+	def determine_color(self):
+		if self.hit_max == 1:
+			block_color = pygame.Color(220,114,42,a=200)
+		elif self.hit_max == 2:
+			block_color = pygame.Color(0,0,220,a=200)
+		elif self.hit_max == 3:
+			block_color = pygame.Color(42,220,114,a=200)
+		return block_color
 
 	def create_block(self):
-		image = pygame.Surface(Block.size).convert_alpha()
+		image = pygame.Surface(self.size).convert_alpha()
 		image.fill(transparent)
 		img_rect = image.get_rect()
-		pygame.draw.rect(image, Block.block_color, img_rect)
+		pygame.draw.rect(image, self.block_color, img_rect)
 		return image
 
 	def hit(self):
 		self.hit_count += 1
-		print(self.hit_count)
 		self.destroy()
 
 	def destroy(self):
 		if self.hit_count == self.hit_max:
 			self.kill()
-			print("killed?")
 
+class Level(object):
+	def __init__(self):
+		self.halfcol = pixelx/2
+		self.level_layout = None
+		self.background = self.create_background()
+		self.ball = None
+		self.paddles = pygame.sprite.Group()
+		self.blocks = pygame.sprite.Group()
+		self.all_sprites = pygame.sprite.Group()
+
+	def collision_check(self):
+		collisions = pygame.sprite.spritecollide(self.ball, self.all_sprites, False)
+		for objects in collisions:
+			self.ball.bounce(objects)
+
+	def create_background(self):
+		self.background = pygame.Surface(screen_size)
+		self.background = self.background.convert()
+		self.background.fill((50,50,50))
+		return self.background
+
+	def choose_level(self, level=1):
+		if level == 1:
+			self.level_layout = [ # 26 * 26
+				"                          ",
+				"                  B       ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"  1111111111111111111111  ",
+				"/  111111111111111111111  /",
+				"  1111111111111111111111  ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"          P               ",
+				"                          ",
+			]
+		if level == 2:
+			self.level_layout = [
+				"                          ",
+				"                  B       ",
+				"                          ",
+				"                          ",
+				"  2222222222222222222222  ",
+				"  2                    2  ",
+				"  2                    2  ",
+				"  2 111111111111111111 2  ",
+				"  2/1111111111111111111/2  ",
+				"  2 111111111111111111 2  ",
+				"  2                    2  ",
+				"  2                    2  ",
+				"  2222222222222222222222  ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"                          ",
+				"          P               ",
+				"                          ",
+			]
+
+	def build_level(self):
+		x = 0
+		y = 0
+		for row in self.level_layout:
+			for col in row:
+				half = False
+				if col == "1":
+					block = Block(x,y, Block.one_hit)
+					self.blocks.add(block)
+					self.all_sprites.add(block)
+				elif col == "2":
+					block = Block(x,y, Block.two_hit)
+					self.blocks.add(block)
+					self.all_sprites.add(block)
+				elif col == "3":
+					block = Block(x,y, Block.three_hit)
+					self.blocks.add(block)
+					self.all_sprites.add(block)
+				elif col == "P":
+					paddle = Paddle(x,y)
+					self.all_sprites.add(paddle)
+					self.paddles.add(paddle)
+				elif col == "B":
+					ball = Ball(x,y)
+					self.ball = ball
+					self.all_sprites.add(self.ball)
+				if col == "/":
+					x += self.halfcol
+				else: 
+					x += pixelx
+			y += pixely
+			x = 0
 
 class App(object):
 	pressed_left = False
@@ -165,20 +299,9 @@ class App(object):
 		self.clock = pygame.time.Clock()
 		self.fps = 120
 		self.done = False
-		self.keys = pygame.key.get_pressed()
-		self.ball = Ball(ball_speed, self.screen)
-		self.paddle_offset = 30
-		self.paddle = Paddle(self.screen_rect.centerx, self.screen_rect.bottom - self.paddle_offset)
-		self.block = Block(500, 100, Block.three_hit)
-		self.all_sprites = pygame.sprite.RenderPlain((self.ball, self.block, self.paddle))
-		self.background = self.create_background()
-
-	def create_background(self):
-		self.background = pygame.Surface(self.screen.get_size())
-		self.background = self.background.convert()
-		self.background.fill((50,50,50))
-		return self.background
-
+		self.level = Level()
+		self.level.choose_level(2)
+		self.level.build_level()
 
 	def event_loop(self):
 		for event in pygame.event.get():
@@ -195,23 +318,11 @@ class App(object):
 				elif event.key == pygame.K_RIGHT:
 					App.pressed_right = False
 
-	def contain_paddle(self): # Should probably be in Paddle, but I didn't want to bother connecting screen_rect to Paddle class
-		if self.paddle.rect.left <= 0:
-			App.pressed_left = False
-		elif self.paddle.rect.right >= self.screen_rect.right:
-			App.pressed_right = False
-
-	def bounce(self):
-		if self.block in self.all_sprites:
-			self.ball.bounce(self.block)
-		self.ball.bounce(self.paddle)		
-
 	def update(self):
-		self.bounce()
-		self.all_sprites.update()
-		self.contain_paddle()
-		self.screen.blit(self.background, (0,0))
-		self.all_sprites.draw(self.screen)
+		self.level.all_sprites.update()
+		self.level.collision_check()
+		self.screen.blit(self.level.background, (0,0))
+		self.level.all_sprites.draw(self.screen)
 		pygame.display.flip()
 
 	def main_loop(self):
@@ -219,7 +330,6 @@ class App(object):
 		while not self.done:
 			self.event_loop()
 			self.update()
-			self.ball.screen_edges(self.screen_rect) # Again, like in App.contain_paddle()...Didn't want to pass screen_rect to ball, but probably should
 			App.dt = self.clock.tick(self.fps)/1000.0
 
 def main():
